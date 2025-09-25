140181141eba98237b51bb66a73ba2180b815ffe
scuf77 <vardgesg286@gmail.com>
2025-09-25 23:49:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -711,7 +711,6 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
     try:
         logger.info("Инициализация кулинарного бота...")
